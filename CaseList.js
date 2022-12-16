48f78c7f1bf5a201bf6833dea6b67e8cb5096bda
Mkorewo <korewo.marcel@gmail.com>
2022-12-16 11:46:08 +0100

    ...

---
+++ CaseList.js
@@ -0,0 +1,9 @@
+import { render } from "@testing-library/react";
+import { Component } from "react";
+
+
+export default class CaseList extends Component{
+    constructor(args){
+        super(args);
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import logo from './logo.svg';
+import obrazek from './obrazek.png';
+import questions from './questions.json';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={obrazek} className="App-logo" alt="logo" /> {/* dodawanie obrazka*/}
+        <a> akapit </a> {/* dodawanie akapitu*/}
+        
+        <div>
+          <ol className='questions-list'>
+            <li>element</li>
+            <li>element</li>
+            <li>element</li>
+            <li>element</li>
+          </ol> {/* dodawanie listy*/}
+
+        </div>
+
+        <a href='#'>odnośnik</a> {/* dodawanie odnośnika*/}
+        
       </header>
     </div>
   );
